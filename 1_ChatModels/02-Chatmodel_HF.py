ea6c5b9c6292e1131c9b2726e0fe4115e1107007
shailesh2210 <shaileshgaddam10@gmail.com>
2025-12-13 22:01:31 +0530

    Chatmodels HF

--- 1_ChatModels/02-Chatmodel_HF.py
+++ 1_ChatModels/02-Chatmodel_HF.py
@@ -6,8 +6,7 @@ load_dotenv()
 
 llm = HuggingFaceEndpoint(
     repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation",
-    huggingfacehub_api_token=os.getenv("huggingfacehub_api_token")
+    task="text-generation"
 )
 
 model = ChatHuggingFace(llm=llm)
